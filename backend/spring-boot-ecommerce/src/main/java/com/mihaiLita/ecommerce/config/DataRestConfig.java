d7b6f3ce1dfaeb5b662690e072726d1a3b125b5b
LitaMihai <litamihai13@gmail.com>
2025-04-30 12:55:45 +0300

    - added ToDo file to .gitignore - disabled the POST, PUT, DELETE methods using a config file

---
+++ backend/spring-boot-ecommerce/src/main/java/com/mihaiLita/ecommerce/config/DataRestConfig.java
@@ -0,0 +1,28 @@
+package com.mihaiLita.ecommerce.config;
+
+import com.mihaiLita.ecommerce.entity.Product;
+import com.mihaiLita.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        // disable HTTP methods for Product and ProductCategory: POST, PUT and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+    }
+}
