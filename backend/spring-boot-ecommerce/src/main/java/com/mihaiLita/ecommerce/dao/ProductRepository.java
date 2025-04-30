430fa5671785e44f98be780cb359fd9ce6ae6abe
LitaMihai <litamihai13@gmail.com>
2025-04-30 14:35:01 +0300

    - allowed CrossOrigin from localhost:4200 (frontend)
    - removed unused import statement

--- backend/spring-boot-ecommerce/src/main/java/com/mihaiLita/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/mihaiLita/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,8 @@ package com.mihaiLita.ecommerce.dao;
 
 import com.mihaiLita.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
