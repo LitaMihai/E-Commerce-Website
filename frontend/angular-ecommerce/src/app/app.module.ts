479cd04f711788ca2a0c3cd25ec219a42e0edb6c
LitaMihai <litamihai13@gmail.com>
2025-05-06 10:14:33 +0300

    - Search for Products by Category and Keyword - Product Detail View - Pagination - Shopping Cart Status - Shopping Cart Items CRUD

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -4,19 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import {ProductService} from './services/product.service';
-import {NgOptimizedImage} from '@angular/common';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { NgOptimizedImage } from '@angular/common';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent,
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgOptimizedImage,
+    NgbModule,
   ],
   providers: [provideHttpClient(withInterceptorsFromDi()), ProductService],
   bootstrap: [AppComponent]
